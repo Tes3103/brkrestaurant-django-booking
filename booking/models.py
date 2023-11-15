580d550245746c3633172ebc0c99da9c3158bc69
Tes3103 <>
2023-11-15 19:59:40 +0000

    Update booking class with new doc string

--- booking/models.py
+++ booking/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 
 class Table(models.Model):
-    code = models.CharField(max_length=80, null=False, blank=False, unique=True)
+    code = models.CharField(max_length=80, null=False,
+                            blank=False, unique=True)
     no_of_gusts = models.IntegerField(null=False, blank=False, default=1)
 
     def _str_(self):
@@ -12,23 +14,32 @@ class Table(models.Model):
 
 
 class Booking(models.Model):
+    user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
     customer_full_name = models.CharField(
         max_length=200, null=False, blank=False)
+    booking_date_and_time = models.DateTimeField(null=True)
+
+    def validate_date(reservation_date_and_time):
+        if booking_date_and_time < timezone.now():
+            raise ValidationError("Date should be in the future")
+    booking_date_and_time = models.DateTimeField(
+        null=True,
+        blank=True,
+        validators=[validate_date])
+
     customer_email = models.EmailField(max_length=100, null=False, blank=False)
-    date = models.DateField(auto_now=True)
-    start_time = models.TimeField()
-    end_time = models.TimeField()
     table = models.ForeignKey(
         Table, on_delete=models.CASCADE, related_name='booking_spot', blank=False)
     created_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['date', 'start_time']
+        unique_together = ('user', 'customer_full_name',
+                           'booking_date_and_time')
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return self.table
-
-
-class BookingQuery(models.Model):
-    """Model for booking query form"""
-    date = models.DateField()
+        return f' User {self.user} has made a booking \
+                   for {self.customer_name}\
+                   for {self.number_of_customers} customers\
+                   for {self.reservation_date_and_time}.'
