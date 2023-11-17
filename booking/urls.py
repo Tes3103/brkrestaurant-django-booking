cff9acbd44072dda5896395c49e1830231f14316
Tes3103 <>
2023-11-17 19:32:16 +0000

    Create urls.py folder in the booking app and add urlpatterns

---
+++ booking/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from restaurant import views
+
+
+urlpatterns = [
+    path('', views.view_home, name='home'),
+    path('add_booking/', views.add_booking, name='add_booking'),
+    path('view_booking/', views.view_booking, name='view_booking'),
+    path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
+    path('delete/<booking_id>', views.delete_booking, name='delete_booking'),
+    path('contact', views.contact, name='contact'),
+
+]
